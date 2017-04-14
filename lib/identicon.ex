aeeedd54e40d826e9664ba1e90d0517ab180eb9c
Jonathan Clarke <jonathan@vayu.com.au>
2017-04-14 13:13:46 +0545

    Computer md5 hash based on string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,29 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Generating random identicon images based on string input
   """
 
   @doc """
-  Hello world.
-
+  main
   ## Examples
 
-      iex> Identicon.hello
-      :world
-
+      iex> Identicon.main("jonathan")
+      [120, 132, 40, 21, 36, 131, 0, 250, 106, 231, 159, 119, 118, 165, 8, 10]
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_main
+  end
+
+  @doc """
+  hash_main
+  ## Examples
+
+      iex> Identicon.hash_main("jonathan")
+      [120, 132, 40, 21, 36, 131, 0, 250, 106, 231, 159, 119, 118, 165, 8, 10]
+  """
+  def hash_main(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list    
   end
 end
